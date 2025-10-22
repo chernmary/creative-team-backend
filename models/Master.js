38d86e52d3669ce32f5f7f0f9f10e0dce7a21e7f
chernmary <chernmary@gmail.com>
2025-10-22 14:04:28 +0300

    Update Master.js

--- models/Master.js
+++ models/Master.js
@@ -1,1 +1,14 @@
+import { DataTypes } from "sequelize";
+import { sequelize } from "../db.js";
+
+export const Master = sequelize.define("Master", {
+  full_name: { type: DataTypes.STRING, allowNull: false },
+  telegram_nick: { type: DataTypes.STRING },
+  phone: { type: DataTypes.STRING },
+  roles: { type: DataTypes.ARRAY(DataTypes.STRING) },
+  notes: { type: DataTypes.TEXT },
+  availability: { type: DataTypes.STRING },
+  privileges: { type: DataTypes.STRING },
+  medbook_expiry: { type: DataTypes.DATEONLY }
+});
 
