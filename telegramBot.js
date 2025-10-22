4150245fa181d0b19c0e32c90029dcfe035706cd
chernmary <chernmary@gmail.com>
2025-10-22 14:05:42 +0300

    Update telegramBot.js

--- telegramBot.js
+++ telegramBot.js
@@ -1,1 +1,14 @@
+import TelegramBot from "node-telegram-bot-api";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: false });
+
+export const sendTelegramMessage = async (message) => {
+  try {
+    await bot.sendMessage(process.env.CHAT_ID, message);
+  } catch (err) {
+    console.error("Ошибка отправки в Telegram:", err.message);
+  }
+};
 
